b1e3a9c20fedb4c7e7939b8603cd2b0aa3ab0a10
Peter Meister (external) <pmeister@izblisbon.campus.unibe.ch>
2019-10-03 14:04:40 +0200

    bining barcodes

---
+++ 03_runBinBarcodes.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+## Allocate resources
+#SBATCH --mail-type=end,fail
+#SBATCH --mail-user=jennifer.semple@izb.unibe.ch
+#SBATCH --time=5-00:00:00
+#SBATCH --partition=all
+#SBATCH --gres=gpu:1
+#SBATCH --mem=96G
+#SBATCH --ntasks=24
+
+
+## job name
+#SBATCH --job-name="binbarcodes"
+
+source ./varSettings.sh
+source ${CONDA_ACTIVATE} BASIC_ONT
+
+echo ${singleFast5_DIR}" is my single_fast5 folder"
+echo ${fastq_DIR}" is my fastq folder"
+echo ${bcFastq_DIR}" is my bcFastq folder"
+
+##################
+# classify reads by barcode
+##################
+echo "Using Deepbinner from:"
+which deepbinner
+classifications=${WORK_DIR}/classifications
+echo $SLURM_NTASKS_PER_NODE
+deepbinner classify --omp_num_threads ${SLURM_NTASKS} --intra_op_parallelism_threads ${SLURM_NTASKS} --inter_op_parallelism_threads 4  --native ${singleFast5_DIR} >  ${classifications}
+
+##################
+# bin by barcode
+##################
+cat ${fastq_DIR}/pass/* > ${fastq_DIR}/pass/passed.fastq.gz
+deepbinner bin --classes ${classifications} --reads ${fastq_DIR}/pass/passed.fastq.gz --out_dir ${bcFastq_DIR}/pass
+rm ${fastq_DIR}/pass/passed.fastq.gz
+
