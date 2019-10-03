929fd7f86df1cdf7a7ba7e7aa1c701c741ccfe97
Peter Meister (external) <pmeister@izblisbon.campus.unibe.ch>
2019-10-03 11:24:38 +0200

    multifast5_DIR

--- varSettings_example.sh
+++ varSettings_example.sh
@@ -3,4 +3,5 @@
 expName=Intestine_DamID
 
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+multiFast5_DIR=${WORK_DIR}/multi_fast5
 singleFast5_DIR=${WORK_DIR}/single_fast5
