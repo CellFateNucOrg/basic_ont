f596c56cca33ed17884fb83283c9ea200a5acc31
Peter Meister (external) <pmeister@izblisbon.campus.unibe.ch>
2019-10-03 11:23:14 +0200

    basecalling in one script

--- 02_runBasecallGuppy.sh
+++ 02_runBasecallGuppy.sh
@@ -8,4 +8,37 @@
 ## job name
 #SBATCH --job-name="guppy&qc"
 
-./basecallGuppy.sh
+## Script to basecall all fast5 in the folder one level up called singleFast5_DIR (named in varSettings.sh) 
+## and its recursive directories with Guppy
+## MUST enter the correct settings in varSettings.sh file
+## (copy varSettings_example.sh to varSettings.sh and then edit it)
+## .bashrc should include location of bin directory inside Guppy directory and export it as ${GUPPY_DIR}
+## .bashrc should include location of conda activate script and export it s ${CONDA_ACTIVATE}
+## outputs 200,000 reads per fastq file
+## output will be in: ./fastqFiles/pass/
+## pycoQC is run and results are in ./qc/
+
+source ./varSettings.sh
+WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "working directory is: "${WORK_DIR}
+source ${CONDA_ACTIVATE} BASIC_ONT
+
+mkdir -p ${WORK_DIR}/fastqFiles
+${GUPPY_DIR}/guppy_basecaller --input_path ${singleFast5_DIR} --save_path ${WORK_DIR}/fastqFiles -c ${GUPPY_DIR}/../data/dna_r9.4.1_450bps_hac.cfg --records_per_fastq 200000 --compress_fastq --recursive --qscore_filtering --min_qscore 3 --device auto
+#--num_callers
+
+
+##################
+# run pycoQC
+#################
+
+
+# create qc output directory
+qcDir=./qc
+mkdir -p $qcDir
+
+#https://github.com/a-slide/pycoQC
+pycoQC -f ${WORK_DIR}/fastqFiles/sequencing_summary.txt -o ${qcDir}/pycoQC_${expName}_pass.html --title ""${expName}" passed reads" --min_pass_qual 3
+
+conda deactivate
+
