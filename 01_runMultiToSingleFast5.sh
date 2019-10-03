4970b0c93e272e5fd992f81d00968e314993508f
Peter Meister (external) <pmeister@izblisbon.campus.unibe.ch>
2019-10-03 12:48:46 +0200

    multiToSingleFast5 script

---
+++ 01_runMultiToSingleFast5.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+## Allocate resources
+#SBATCH --time=8-00:00:00
+#SBATCH --partition=all
+#SBATCH --mem-per-cpu=16G
+#SBATCH --cpus-per-task=8
+
+## job name
+#SBATCH --job-name="multi2single_fast5"
+
+source ${CONDA_ACTIVATE} BASIC_ONT
+source ./varSettings.sh
+
+multi_to_single_fast5 --input_path ${multiFast5_DIR}/ --save_path ${singleFast5_DIR}/ -t $SLURM_CPUS_PER_TASK --recursive
+
